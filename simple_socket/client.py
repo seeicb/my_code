4247aac62ccffd4c372e345790b544c8057699f4
cheng_b <seeicb@163.com>
2016-04-24 16:53:23 +0800

    simple socket

---
+++ simple_socket/client.py
@@ -0,0 +1,24 @@
+import socket
+host='127.0.0.1'
+port=12345
+#172.18.63.42
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect((host,port))
+while True:
+    try:
+        cdata=input('输入:')
+        client.send(cdata.encode('utf8'))
+        if cdata=='exit':
+            print('断开连接')
+            break
+        sdata=client.recv(2048)
+        print("B:",sdata.decode('utf8'))
+        if sdata.decode('utf8')=='exit':
+            print('断开连接')
+            break
+    except:
+        print("错误")
+        break
+
+
+client.close()
